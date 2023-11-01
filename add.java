c0e000abf98c7974818daeea38e630e31670333d
Bishal Thakur <thabishal0@gmail.com>
2023-11-02 00:04:38 +0545

    update

--- add.java
+++ add.java
@@ -4,9 +4,12 @@ public class add {
         int a=50;
         int b=30;
         int sum=a+b;
+        int mul=a*b;
         //System.out.println(sum);
         int diff=a-b;
         System.out.println(diff);
+        System.out.println(sum);
+        System.out.println(mul);
     }
     
 }
